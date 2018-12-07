abc4e7cfbc6b154786ec7d718cf5d90d9310a19c
Dan Macumber <dan.macumber@gmail.com>
2018-12-07 12:28:44 -0700

    Remove qprogressbar (#3370)
    * Intermediate commit
    
    * Build succeeds, next need to create a separate openstudio_modeleditor_rb target
    
    * Compiling and can load separate openstudio_modeleditor.so, but can't call Qt widgets in it.  Think I have to move the QApplication into the new module too
    
    * Fixed include
    
    * Moving Application to model_editor, configures
    
    * Compiles after moving Application
    
    * Remove call to init Application in CLI
    
    * Modeleditor classes seem to be working, next step is fix the plugin
    
    * Plugin is working
    
    * Removing unused Qt traces from headers and swig files
    
    * Making a real class for ColorRGB
    
    * Replace Q_ASSERT with OS_ASSERT
    
    * Removing QElapsedTimer in debug log messages

--- OSProgressBar.hpp
+++ OSProgressBar.hpp
@@ -27,26 +27,92 @@
 *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***********************************************************************************************************************/
 
-#include "ModelEditorFixture.hpp"
+#ifndef MODELEDITOR_OSProgressBar_HPP
+#define MODELEDITOR_OSProgressBar_HPP
 
-#include "../model_editor/Application.hpp"
+#include "../utilities/plot/ProgressBar.hpp"
+#include "../utilities/core/Macro.hpp"
+#include "../utilities/core/String.hpp"
 
-#include "../../utilities/core/Path.hpp"
+#include <QProgressBar>
+#include <memory>
 
-void ModelEditorFixture::SetUp() {
-  openstudio::Application::instance().application(true);
-}
+class QWidget;
 
-void ModelEditorFixture::TearDown() {}
+/** OSProgressBar wraps a QProgressBar and provides virtual methods setRange, setValue, and setWindowTitle(QString)
+  *  which may be overridden.
+  *
+  *  OSProgressBar an atypical QObject because it is designed to be stack allocated.  In many cases it
+  *  would be preferred to connect your own heap allocated QObject to the signals directly rather
+  *  than using this convenience class.
+  **/
+class OSProgressBar : public openstudio::ProgressBar {
 
-void ModelEditorFixture::SetUpTestCase() {
-  // set up logging
-  logFile = openstudio::FileLogSink(openstudio::toPath("./ModelEditorFixture.log"));
-  logFile->setLogLevel(Debug);
-  openstudio::Logger::instance().standardOutLogger().disable();
-}
+public:
 
-void ModelEditorFixture::TearDownTestCase(){}
+  /// constructor
+  OSProgressBar(QWidget* parent = nullptr);
 
-// static variables
-boost::optional<openstudio::FileLogSink> ModelEditorFixture::logFile;
+  /// constructor
+  OSProgressBar(bool visible, QWidget* parent = nullptr);
+
+  /// virtual destructor
+  virtual ~OSProgressBar();
+
+  /// get min
+  int minimum() const;
+
+  /// set min
+  void setMinimum(int min);
+
+  /// get max
+  int maximum() const;
+
+  /// set max
+  void setMaximum(int max);
+
+  /// get value
+  int value() const;
+
+  /// get the window title
+  std::string windowTitle() const;
+
+  /// set the window title
+  void setWindowTitle(const std::string& title);
+
+  /// get the text
+  std::string text() const;
+
+  /// get if visible
+  bool isVisible() const;
+
+  /// set if visible
+  void setVisible(bool visible);
+
+  /// virtual method called every time percentageUpdated fires
+  virtual void onPercentageUpdated(double percentage);
+
+// public slots:
+
+  /// set range
+  void setRange(int min, int max);
+
+  /// set value
+  void setValue(int value);
+
+  /// set window title
+  void setWindowTitle(const QString& windowTitle);
+
+protected:
+
+  /// return the impl
+  //std::shared_ptr<QProgressBar> impl() const;
+
+private:
+  /// impl
+  std::shared_ptr<QProgressBar> m_impl;
+
+};
+
+
+#endif //MODELEDITOR_OSProgressBar_HPP
