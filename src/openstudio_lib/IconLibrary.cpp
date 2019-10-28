4bdb894b70dab57be2f3bf80ecdfc214c6e74071
Kyle Benne <kyle.benne@nrel.gov>
2019-10-28 10:16:52 -0500

    App related components of OS Core PRs
    https://github.com/NREL/OpenStudio/pull/3697
    https://github.com/NREL/OpenStudio/pull/3694
    https://github.com/NREL/OpenStudio/pull/3687

--- src/openstudio_lib/IconLibrary.cpp
+++ src/openstudio_lib/IconLibrary.cpp
@@ -69,6 +69,7 @@ namespace openstudio {
 
 IconLibrary::IconLibrary()
 {
+  m_icons[openstudio::IddObjectType(openstudio::IddObjectType::OS_AirConditioner_VariableRefrigerantFlow).value()] = new QPixmap(":images/vrf_outdoor.png");
   m_icons[openstudio::IddObjectType(openstudio::IddObjectType::OS_AirLoopHVAC_OutdoorAirSystem).value()] = new QPixmap(":images/OAMixer.png");
   m_icons[openstudio::IddObjectType(openstudio::IddObjectType::OS_AirLoopHVAC_UnitaryCoolOnly).value()] = new QPixmap(":images/DXCoolingCoil.png");
   m_icons[openstudio::IddObjectType(openstudio::IddObjectType::OS_AirLoopHVAC_UnitaryHeatPump_AirToAir).value()] = new QPixmap(":images/heat_pump3.png");
