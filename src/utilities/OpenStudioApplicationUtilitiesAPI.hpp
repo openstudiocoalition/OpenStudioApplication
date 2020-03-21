acddb619fb0d01ca1da5e63e9119931221fa2d1c
Jason Turner <jason@emptycrate.com>
2020-03-21 14:35:56 -0600

    Fix static builds of OS App libraries
    Thanks @kbenne for the correction
    
    note: this still does not link the ruby bindings for the reasons mentioned in 5cd974bb371253626d9de5d0f5256e2d05739fb6
    
    This also requires a patch to OpenStudio to export some more missing
    symbols https://github.com/NREL/OpenStudio/compare/add_missing_apis_exports?expand=1

--- src/utilities/OpenStudioApplicationUtilitiesAPI.hpp
+++ src/utilities/OpenStudioApplicationUtilitiesAPI.hpp
@@ -30,7 +30,7 @@
 #ifndef OSAPP_UTILITIES_OSAPP_UTILITIESAPI_HPP
 #define OSAPP_UTILITIES_OSAPP_UTILITIESAPI_HPP
 
-#if (_WIN32 || _MSC_VER) && SHARED_OS_LIBS
+#if (_WIN32 || _MSC_VER) && SHARED_OSAPP_LIBS
 
 #ifdef openstudioapp_utilities_EXPORTS
 #define OSAPP_UTILITIES_API __declspec(dllexport)
