5cd974bb371253626d9de5d0f5256e2d05739fb6
Jason Turner <jason@emptycrate.com>
2020-03-20 22:11:44 -0600

    Properly export symbols for built DLL's
    This gets MSVC build *mostly* working. The problem now
    is linking of openstudio_modeleditor_rb. There is
    currently code in place in
    
    ruby/CMakeLists.txt
    
    to ignore missing symbols for ruby dependencies on clang
    and gcc. We can attempt the same thing with -FORCE:UNRESOLVED
    for the msvc linker. However, CMake still reads the errors reported
    as a failure and CMake will not continue.
    
    This can likely be worked around. However, I want to know why
    we have to ignore these linker errors instead of properly linking
    to a provided .lib file form the OpenStudio library?

--- src/utilities/OpenStudioApplicationUtilitiesAPI.hpp
+++ src/utilities/OpenStudioApplicationUtilitiesAPI.hpp
@@ -1,5 +1,5 @@
 /***********************************************************************************************************************
-*  OpenStudio(R), Copyright (c) 2008-2019, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.
+*  OpenStudio(R), Copyright (c) 2008-2020, Alliance for Sustainable Energy, LLC, and other contributors. All rights reserved.
 *
 *  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
 *  following conditions are met:
@@ -27,42 +27,18 @@
 *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ***********************************************************************************************************************/
 
-#ifndef OSAPP_UTILITIES_APPLICATIONPATHHELPERS_HPP
-#define OSAPP_UTILITIES_APPLICATIONPATHHELPERS_HPP
+#ifndef OSAPP_UTILITIES_OSAPP_UTILITIESAPI_HPP
+#define OSAPP_UTILITIES_OSAPP_UTILITIESAPI_HPP
 
-#include "OpenStudioApplicationUtilitiesAPI.hpp"
-#include <openstudio/utilities/core/Path.hpp>
-#include <vector>
+#if (_WIN32 || _MSC_VER) && SHARED_OS_LIBS
 
-namespace openstudio {
+#ifdef openstudioapp_utilities_EXPORTS
+#define OSAPP_UTILITIES_API __declspec(dllexport)
+#else
+#define OSAPP_UTILITIES_API __declspec(dllimport)
+#endif
+#else
+#define OSAPP_UTILITIES_API
+#endif
 
-  /// \returns The source directory the application was built from
-  OSAPP_UTILITIES_API openstudio::path getOpenStudioApplicationSourceDirectory();
-
-  /// \returns The directory the application was built in
-  OSAPP_UTILITIES_API openstudio::path getOpenStudioApplicationBuildDirectory();
-
-  /// \returns The path to the current executable application
-  OSAPP_UTILITIES_API openstudio::path getOpenStudioApplicationPath();
-
-  /// \returns The directory of the current executable application
-  OSAPP_UTILITIES_API openstudio::path getOpenStudioApplicationDirectory();
-
-  /// \returns True if the application is running from the build directory
-  OSAPP_UTILITIES_API bool isOpenStudioApplicationRunningFromBuildDirectory();
-
-  /// \returns Will return path to the binary containing OpenStudio Utilities, could be openstudio.exe, openstudio.so, etc.
-  OSAPP_UTILITIES_API openstudio::path getOpenStudioApplicationModule();
-
-  /// \returns Will return dir containing the binary containing OpenStudio Utilities, could be openstudio.exe, openstudio.so, etc.
-  OSAPP_UTILITIES_API openstudio::path getOpenStudioApplicationModuleDirectory();
-
-  /// \returns True if the OpenStudio Module is running from the build directory
-  OSAPP_UTILITIES_API bool isOpenStudioApplicationModuleRunningFromBuildDirectory();
-
-  /// \returns The path to the OpenStudio Command Line Interface  if it exists.
-  OSAPP_UTILITIES_API openstudio::path getOpenStudioCoreCLI();
-
-} // openstudio
-
-#endif //OSAPP_UTILITIES_APPLICATIONPATHHELPERS_HPP
+#endif
