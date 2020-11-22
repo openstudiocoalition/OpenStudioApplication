0c05c1c4a74e6db4ef0ce3fa8de6cc9c5d4ce23d
Dan Macumber <dan.macumber@gmail.com>
2020-11-21 18:15:54 -0700

    Install Qt as a Framework in Ruby

--- SketchUpInstallName.rb
+++ SketchUpInstallName.rb
@@ -1,18 +1,19 @@
 def fixup(file, rpath_replacement)
   puts
   puts file
-  `otool -L #{file}`.split("\n").each do |line|
-    line.strip!
-    if md = /(@rpath[^\s]*)/.match(line)
-      line = md[1]
-      new_line = line.gsub("@rpath", rpath_replacement)
-      if md = /Qt.*\/(Qt.*?)$/.match(new_line)
-      	new_line = new_line.gsub(md[0], md[1])
-      end
-      puts "#{line} -> #{new_line}"
-     `install_name_tool -change #{line} #{new_line} #{file}`
-    end
-  end
+  `install_name_tool -add_rpath #{rpath_replacement} #{file}`
+  #`otool -L #{file}`.split("\n").each do |line|
+  #  line.strip!
+  #  if md = /(@rpath[^\s]*)/.match(line)
+  #    line = md[1]
+  #    new_line = line.gsub("@rpath", rpath_replacement)
+  #    if md = /Qt.*\/(Qt.*?)$/.match(new_line)
+  #    	#new_line = new_line.gsub(md[0], md[1])
+  #    end
+  #    puts "#{line} -> #{new_line}"
+  #   `install_name_tool -change #{line} #{new_line} #{file}`
+  #  end
+  #end
 end
 
 # Fix up any load paths in openstudio_modeleditor.bundle
@@ -23,14 +24,17 @@ def fixup_model_editor(lib_path, lib_path_replacemant, openstudio_modeleditor_pa
 end
 
 dir = File.dirname(ARGV[0])
-fixup(ARGV[0], "@loader_path")
-fixup(File.join(dir, "QtConcurrent"), "@loader_path")
-fixup(File.join(dir, "QtCore"), "@loader_path")
-fixup(File.join(dir, "QtGui"), "@loader_path")
-fixup(File.join(dir, "QtNetwork"), "@loader_path")
-fixup(File.join(dir, "QtPrintSupport"), "@loader_path")
-fixup(File.join(dir, "QtWidgets"), "@loader_path")
-fixup(File.join(dir, "QtXml"), "@loader_path")
+
+fixup(ARGV[0], "@loader_path/")
+
+qt_libs = ['QtConcurrent', 'QtCore', 'QtGui', 'QtNetwork','QtPrintSupport', 'QtWidgets', 'QtXml']
+qt_libs.each do |qt_lib|
+  Dir.glob(File.join(dir, '**', qt_lib)).each do |file|
+    next if /Headers/.match(file)
+    fixup(file, "@loader_path/")
+  end
+end
+
 fixup(File.join(dir, "platforms/libqcocoa.dylib"), "@loader_path/..")
 
 fixup_model_editor("libopenstudiolib.dylib", "@loader_path/libopenstudiolib.dylib", File.join(dir, "openstudio_modeleditor.bundle"))
