715559bf759e4ab8df47448ecd15cbd9795509b6
Julien Marrec <julien.marrec@gmail.com>
2021-06-01 16:33:32 +0200

    Adjust precommit hook: just stash unstaged changed, run clang format, then pop the stash

--- ci/pre-commit.sh
+++ ci/pre-commit.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-# at root of repo: cd .git/hooks && ln -s ../../ci/pre-commit.sh pre-commit
+# Installing the hook: at root of repo: cd .git/hooks && ln -s ../../ci/pre-commit.sh pre-commit
+
+# stash unstaged changes
+git stash -k -u
+# Run clang format
 ./ci/clang-format.sh HEAD develop
+# Pop stash
+git stash pop
