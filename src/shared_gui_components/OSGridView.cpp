88ab72336a96bcaa6491f83f4803bbc6e7267bb1
Dan Macumber <dan.macumber@gmail.com>
2022-07-03 09:23:17 -0600

    Fix too bold H3 and add minimum size to grid view category buttons

--- src/shared_gui_components/OSGridView.cpp
+++ src/shared_gui_components/OSGridView.cpp
@@ -126,6 +126,7 @@ OSGridView::OSGridView(OSGridController* gridController, const QString& headerTe
   for (unsigned i = 0; i < categories.size(); i++) {
     auto button = new QPushButton(categories.at(i));
     button->setObjectName("CategoryButton");
+    button->setMinimumWidth(100);
     button->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::MinimumExpanding);
     button->setCheckable(true);
     buttonLayout->addWidget(button);
