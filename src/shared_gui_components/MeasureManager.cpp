2d817c30988ea75b04b00308b2c71c5e7e249bce
Julien Marrec <julien.marrec@gmail.com>
2023-08-16 20:42:08 +0200

    Cleanup QNetwork call prep. Use actual QJsonObjects instead of doing string interpolation to get a QByteArray

--- src/shared_gui_components/MeasureManager.cpp
+++ src/shared_gui_components/MeasureManager.cpp
@@ -76,6 +76,8 @@
 #include <QNetworkRequest>
 #include <QNetworkReply>
 #include <QThread>
+#include <QJsonObject>
+#include <QJsonDocument>
 // Debug only
 //#include <QSslError>
 //#include <QDateTime>
@@ -102,15 +104,15 @@ bool MeasureManager::waitForStarted(int msec) {
 
   QUrl thisUrl(m_url);
   thisUrl.setPath("/");
+  QNetworkRequest request(thisUrl);
+  request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-  int msecPerLoop = 20;
-  int numTries = msec / msecPerLoop;
+  QNetworkAccessManager manager;
+
+  const int msecPerLoop = 20;
+  const int numTries = msec / msecPerLoop;
   int current = 0;
   while (!success && current < numTries) {
-    QNetworkRequest request(thisUrl);
-    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");  // TODO: should probably move that out of the loop
-
-    QNetworkAccessManager manager;
 
     QNetworkReply* reply = manager.get(request);
 
@@ -461,21 +463,24 @@ std::vector<measure::OSArgument> MeasureManager::getArguments(const BCLMeasure& 
 
   // std::string url_s = m_url.toString().toStdString();
 
-  QString data = QString(R"json({"measure_dir": "%1", "osm_path": "%2"})json").arg(toQString(t_measure.directory()), toQString(m_tempModelPath));
-
   QNetworkRequest request(thisUrl);
   request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-  QNetworkAccessManager manager;
+  QJsonObject obj;
+  obj["measure_dir"] = toQString(t_measure.directory());
+  obj["osm_path"] = toQString(m_tempModelPath);
+  const QJsonDocument doc(obj);
+  const QByteArray data = doc.toJson();
 
-  QNetworkReply* reply = manager.post(request, data.toUtf8());
+  QNetworkAccessManager manager;
+  QNetworkReply* reply = manager.post(request, data);
 
   while (reply->isRunning()) {
     Application::instance().processEvents();
   }
 
-  QString replyString = reply->readAll();
-  std::string s = replyString.toStdString();
+  const QString replyString = reply->readAll();
+  const std::string s = replyString.toStdString();
   auto error = reply->error();
   delete reply;
 
@@ -838,14 +843,13 @@ bool MeasureManager::reset() {
 
   // std::string url_s = m_url.toString().toStdString();
 
-  QString data = QString("{}");
-
   QNetworkRequest request(thisUrl);
   request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-  QNetworkAccessManager manager;
+  const QByteArray data = QString("{}").toUtf8();
 
-  QNetworkReply* reply = manager.post(request, data.toUtf8());
+  QNetworkAccessManager manager;
+  QNetworkReply* reply = manager.post(request, data);
 
   while (reply->isRunning()) {
     Application::instance().processEvents();
@@ -878,14 +882,13 @@ bool MeasureManager::checkForLocalBCLUpdates() {
 
   // std::string url_s = m_url.toString().toStdString();
 
-  QString data = QString("{}");
-
   QNetworkRequest request(thisUrl);
   request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-  QNetworkAccessManager manager;
+  const QByteArray data = QString("{}").toUtf8();
 
-  QNetworkReply* reply = manager.post(request, data.toUtf8());
+  QNetworkAccessManager manager;
+  QNetworkReply* reply = manager.post(request, data);
 
   while (reply->isRunning()) {
     Application::instance().processEvents();
@@ -918,15 +921,17 @@ bool MeasureManager::checkForUpdates(const openstudio::path& measureDir, bool fo
 
   // std::string url_s = m_url.toString().toStdString();
 
-  QString data =
-    QString(R"json({"measure_dir": "%1", "force_reload": "%2"})json").arg(toQString(measureDir), force ? QString("true") : QString("false"));
+  QJsonObject obj;
+  obj["measure_dir"] = toQString(measureDir);
+  obj["force_reload"] = force;
+  const QJsonDocument doc(obj);
+  const QByteArray data = doc.toJson();
 
   QNetworkRequest request(thisUrl);
   request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
   QNetworkAccessManager manager;
-
-  QNetworkReply* reply = manager.post(request, data.toUtf8());
+  QNetworkReply* reply = manager.post(request, data);
 
   while (reply->isRunning()) {
     Application::instance().processEvents();
