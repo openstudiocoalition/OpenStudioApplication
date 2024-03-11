9665a371c880f37fb6c863c46ee2ca94998faf73
Julien Marrec <julien.marrec@gmail.com>
2024-03-11 11:02:07 +0100

    Use the newer gmp rev for all platforms, should be removed later

--- ConanInstall.cmake
+++ ConanInstall.cmake
@@ -41,13 +41,8 @@ if(NOT CONAN_OPENSTUDIO_ALREADY_RUN)
   # TODO: temp, revert to stable soon
   set(CONAN_RUBY "openstudio_ruby/2.7.2@nrel/testing#d66e3b66568b13acf3b16d866bec68d0")
 
-  # see issue #689
-  if(WIN32)
-    set(CONAN_GMP "")
-  else()
-    conan_add_remote(NAME conancenter INDEX 1 URL https://center.conan.io)
-    set(CONAN_GMP "gmp/6.2.1#560eab30a8f52beae2cef5ff26cdd61c")
-  endif()
+  # TODO: temp, see issue #689 . gmp revision on NREL has sources to gmplib.org which banned GHA IP range, so pick a newer conan-center-index revision in the meantime that uses GCC's FTP.
+  set(CONAN_GMP "gmp/6.2.1#560eab30a8f52beae2cef5ff26cdd61c")
 
   # conan_add_remote(
   #   NAME bincrafters URL
