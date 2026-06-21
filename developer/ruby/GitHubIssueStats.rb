9b88e705017d3262e908efb155b57b92540adc3c
Dan Macumber <dan.macumber@gmail.com>
2026-06-21 11:34:26 -0600

    Fix release notes for next RC

--- developer/ruby/GitHubIssueStats.rb
+++ developer/ruby/GitHubIssueStats.rb
@@ -1,4 +1,4 @@
-require 'github_api'
+require 'octokit'
 require 'date'
 require 'yaml'
 
@@ -6,20 +6,18 @@ require 'yaml'
 # that is at least a week old
 def get_begin_date_and_previous_tag()
   a_week_ago = Time.now - (60*60*24*7)
+  repo = "#{@repo_owner}/#{@repo}"
 
-  @github.repos.releases.list(owner: @repo_owner,
-                              repo: @repo).each_page do |page|
-    page.each do |release|
-      next if release.tag_name !~ /^v\d\.\d\.0$/
-      release_date = Time.parse(release.created_at)
-      next if release_date > a_week_ago
-      # This is perhaps unecessary since we match to a tag in vX.Y.0 format
-      # already but it doesn't hurt
-      next if release.prerelease
-      next if release.draft
-      STDERR.puts "Found previous major/minor release: #{release.tag_name}, #{release_date}"
-      return release_date, " (#{release.tag_name})"
-    end
+  @github.releases(repo, per_page: 100).each do |release|
+    next if release.tag_name !~ /^v\d+\.\d+\.0$/
+    release_date = release.created_at.to_time
+    next if release_date > a_week_ago
+    # This is perhaps unecessary since we match to a tag in vX.Y.0 format
+    # already but it doesn't hurt
+    next if release.prerelease
+    next if release.draft
+    STDERR.puts "Found previous major/minor release: #{release.tag_name}, #{release_date}"
+    return release_date, " (#{release.tag_name})"
   end
   STDERR.puts "Cannot find previous release, setting time to 2005"
   return Time.new(2005, 01, 01), ""
@@ -60,15 +58,17 @@ end
 
 if !ENV['GITHUB_TOKEN'].nil?
   token = ENV['GITHUB_TOKEN']
-  @github = Github.new oauth_token: token
+  @github = Octokit::Client.new(access_token: token)
 elsif File.exist?(Dir.home + '/github_config.yml')
   github_options = YAML.load_file(Dir.home + '/github_config.yml')
   token = github_options['oauth_token']
-  @github = Github.new oauth_token: token
+  @github = Octokit::Client.new(access_token: token)
 else
   STDERR.puts "Github Token not found"
-  @github = Github.new
+  @github = Octokit::Client.new
 end
+
+@github.auto_paginate = true
 
 @begin_date, @prev_tag = get_begin_date_and_previous_tag()
 
@@ -78,59 +78,35 @@ newIssues = Array.new
 closedIssues = Array.new
 acceptedPullRequests = Array.new
 
-# Process Open Issues
-results = -1
-page = 1
-while (results != 0)
-  resp = @github.issues.list user: @repo_owner, repo: @repo,
-                            :sort => 'created',
-                            :direction => 'asc',
-                            :state => 'open',
-                            :per_page => 100,
-                            :page => page
-  results = resp.length
-  resp.env[:body].each do |issue, index|
-    created = Time.parse(issue.created_at)
-    if !issue.has_key?(:pull_request)
-      totalOpenIssues << issue
-      if created >= @begin_date && created <= @end_date
-        newIssues << issue
-      end
-    else
-      totalOpenPullRequests << issue
-    end
-  end
+repo = "#{@repo_owner}/#{@repo}"
 
-  page = page + 1
+# Process Open Issues
+@github.issues(repo, sort: 'created', direction: 'asc', state: 'open', per_page: 100).each do |issue|
+  created = issue.created_at.to_time
+  if issue.pull_request.nil?
+    totalOpenIssues << issue
+    if created >= @begin_date && created <= @end_date
+      newIssues << issue
+    end
+  else
+    totalOpenPullRequests << issue
+  end
 end
 
 # Process Closed Issues
-results = -1
-page = 1
-while (results != 0)
-  resp = @github.issues.list user: @repo_owner, repo: @repo,
-                            :sort => 'created',
-                            :direction => 'asc',
-                            :state => 'closed',
-                            :per_page => 100,
-                            :page => page
-  results = resp.length
-  resp.env[:body].each do |issue, index|
-    created = Time.parse(issue.created_at)
-    closed = Time.parse(issue.closed_at)
-    if !issue.has_key?(:pull_request)
-      if created >= @begin_date && created <= @end_date
-        newIssues << issue
-      end
-      if closed >= @begin_date && closed <= @end_date
-        closedIssues << issue
-      end
-    elsif closed >= @begin_date && closed <= @end_date
-      acceptedPullRequests << issue
+@github.issues(repo, sort: 'created', direction: 'asc', state: 'closed', per_page: 100).each do |issue|
+  created = issue.created_at.to_time
+  closed = issue.closed_at.to_time
+  if issue.pull_request.nil?
+    if created >= @begin_date && created <= @end_date
+      newIssues << issue
     end
+    if closed >= @begin_date && closed <= @end_date
+      closedIssues << issue
+    end
+  elsif closed >= @begin_date && closed <= @end_date
+    acceptedPullRequests << issue
   end
-
-  page = page + 1
 end
 
 closedIssues.sort! {|x,y| get_num(x) <=> get_num(y)}
